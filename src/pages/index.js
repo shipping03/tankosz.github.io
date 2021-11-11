5e67d7bb567054e4ad64f0b6f5d1734b87c572cb
shawn <shipping03@marskt.com>
2021-11-11 09:41:49 +0800

    update

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Home() {
-  return <div style={{ color: 'blue' }}>
-    <h1>网站正加班建设中!</h1>
-    <p>What a  world.</p>
-    <img src="https://source.unsplash.com/random/790x1100" alt="" />
-  </div>
-}
