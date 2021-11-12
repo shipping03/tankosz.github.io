befad5aab83f8c407a5c347da8171564130e6fdb
shawn <shipping03@marskt.com>
2021-11-12 18:58:08 +0800

    update

--- component---src-pages-index-js-fec7c6e36553fba2d710.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[678],{704:function(e,t,l){l.r(t),l.d(t,{default:function(){return n}});var r=l(294);function n(){return r.createElement("div",{style:{color:"blue"}},r.createElement("h1",null,"网站正加班建设中!"),r.createElement("p",null,"What a  world."),r.createElement("img",{src:"https://source.unsplash.com/random/790x1100",alt:""}))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-fec7c6e36553fba2d710.js.map
