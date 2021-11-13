df0b9cc9266aed9f1268274b25b877c094f9bd1c
shawn <shipping03@marskt.com>
2021-11-13 18:19:28 +0800

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
-import github from "../img/github-icon.svg";
+// import github from "../img/github-icon.svg";
 import logo from "../img/logo.svg";
 
 const Navbar = class extends React.Component {
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+              <img src={logo} alt="Tanko" style={{ width: "88px" }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -64,22 +64,22 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                关于腾柯
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                腾柯产品
               </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
+                公司动态
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
+                联系我们
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
+             {/* <Link className="navbar-item" to="/contact/examples">
                 Form Examples
-              </Link>
+              </Link>*/}
             </div>
-            <div className="navbar-end has-text-centered">
+            {/*<div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
                 href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
@@ -90,7 +90,7 @@ const Navbar = class extends React.Component {
                   <img src={github} alt="Github" />
                 </span>
               </a>
-            </div>
+            </div>*/}
           </div>
         </div>
       </nav>
