1fab69e6a9af0df2f972cde84e1e14a5100ae687
shawn <shipping03@marskt.com>
2021-11-17 18:36:02 +0800

    add

---
+++ public/component---src-pages-404-js-517aadde16e7dc7b6873.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_netlify_cms=self.webpackChunkgatsby_starter_netlify_cms||[]).push([[883],{9616:function(e,t,n){n.r(t);var l=n(7294),s=n(5310);t.default=function(){return l.createElement(s.Z,null,l.createElement("div",null,l.createElement("h1",null,"NOT FOUND"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-517aadde16e7dc7b6873.js.map
